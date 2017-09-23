5532eff172c84daa1263c51f0ca011f02f82dfbe
KnarManukyan <Knarik_Manukyan@edu.aua.am>
2017-09-23 18:48:00 +0400

    first commit

---
+++ homework3.js
@@ -0,0 +1,49 @@
+
+
+const func1 = function(a,b,c){
+  console.log(a+b+c);
+  return a+b+c;
+  }
+
+//alert(func1(5,6,7));
+const func2= function(){
+  const volume=55;
+  return volume;
+}
+//alert(func2());
+const func3= function(x,y){
+const sum=x+y;
+
+
+  }
+  alert(func3(5,6))
+  const func4= function(name,last_name)
+  {
+
+    return name+" "+last_name;
+  }
+const func5= function(x,y,z){
+if(x.lenght>=y.length && x.lenght>=z.lenght){
+return x;
+}
+else if(y.lenght>=x.lenght && y.lenght>=z.lenght){
+return y;
+}
+else {
+  return z;
+}
+}
+const func6= function(x,y){
+  if(x===y){
+    return 0;
+  }
+  else if(x>y){
+    return 1;
+  }
+  else{
+    return -1
+  }
+}
+const func7= function(x,y,z){
+return x||y||z;
+}
